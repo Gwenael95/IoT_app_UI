74df3365d50202b16b95754deb85fe94897f6fa9
Railas9 <railasben@gmail.com>
2022-05-04 17:40:55 +0200

    Add firebase to app and insert into database start of the party

--- app/src/main/java/com/geta/buttonwar/MainActivity.java
+++ app/src/main/java/com/geta/buttonwar/MainActivity.java
@@ -1,11 +1,24 @@
 package com.geta.buttonwar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,9 +28,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.btn);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                Map<String, Object> party = new HashMap<>();
+                party.put("dateParty", LocalDateTime.now());
+                party.put("dureeParty", 60);
+                party.put("scoreJ1", 0);
+                party.put("scoreJ2", 0);
+                party.put("scoreJ3", 0);
+                db.collection("Partie")
+                        .add(party)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.i("added", "DocumentSnapshot added with ID: ");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.i("TAG", "Error adding document", e);
+                            }
+                        });
+
                 Intent intent = new Intent(MainActivity.this, GameScreen.class);
                 startActivity(intent);
             }
