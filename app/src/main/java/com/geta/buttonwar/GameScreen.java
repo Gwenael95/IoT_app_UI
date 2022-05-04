6d6107ba08b22b23c7603d6fedb938934dd02d19
Freddy <begarin.freddy@gmail.com>
2022-05-04 15:01:57 +0200

    timer

--- app/src/main/java/com/geta/buttonwar/GameScreen.java
+++ app/src/main/java/com/geta/buttonwar/GameScreen.java
@@ -3,6 +3,8 @@ package com.geta.buttonwar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 
 public class GameScreen extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class GameScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
+        TextView gameTime= findViewById(R.id.gameTime);
+         new CountDownTimer(60000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                gameTime.setText("seconds remaining: " + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                gameTime.setText("done!");
+            }
+        }.start();
+
+
     }
+
+
+
 }
--- app/src/main/java/com/geta/buttonwar/MainActivity.java
+++ app/src/main/java/com/geta/buttonwar/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.renderscript.ScriptGroup;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,12 +18,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button startButton= findViewById(R.id.button);
+        //TextView gameTime= findViewById(R.id.gameTime);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Intent intent = new Intent(MainActivity.this, GameScreen.class);
-
                 startActivity(intent);
             }
         });
