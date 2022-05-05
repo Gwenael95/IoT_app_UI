c78c97c9a66444c844749b593a92ea3cfff7f0b5
Railas9 <railasben@gmail.com>
2022-05-05 14:25:39 +0200

    rejouer works

--- app/src/main/java/com/geta/buttonwar/ResultActivity.java
+++ app/src/main/java/com/geta/buttonwar/ResultActivity.java
@@ -1,5 +1,6 @@
 package com.geta.buttonwar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,6 +9,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -20,11 +30,32 @@ public class ResultActivity extends AppCompatActivity {
         int PtJoueur2 = getIntent().getIntExtra("scoreJ2", 0);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
         button = findViewById(R.id.replay);
         resulat = findViewById(R.id.result);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Map<String, Object> party = new HashMap<>();
+                party.put("dateParty", LocalDateTime.now());
+                party.put("dureeParty", 60);
+                party.put("scoreJ1", 0);
+                party.put("scoreJ2", 0);
+                party.put("scoreJ3", 0);
+                db.collection("Partie")
+                        .add(party)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.i("added", "DocumentSnapshot added with ID: ");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.i("TAG", "Error adding document", e);
+                            }
+                        });
                 Intent intent = new Intent(ResultActivity.this, GameScreen.class);
                 startActivity(intent);
             }
